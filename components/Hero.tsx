ceb5a608903b5a9a6323f07251819f6a84d7e4c9
Afeez Awoyemi <damafeez@gmail.com>
2023-01-13 00:00:13 +0100

    feat: bootstrap hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <header className="flex py-24">
+      <div className=" max-w-3xl">
+        <h2 className=" text-foreground-secondary">-- Afeez Awoyemi</h2>
+        <h1 className=" text-3xl">
+          building intuitive
+          <br /> and user-friendly applications.
+        </h1>
+      </div>
+    </header>
+  )
+}
