b66b51207a386b54f64ed3e0701ad492bdafc225
Afeez Awoyemi <damafeez@gmail.com>
2023-01-12 23:59:01 +0100

    chore: perfect tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        current: 'currentColor',
+        'foreground-secondary': 'rgb(var(--foreground-secondary-rgb))',
+      },
+      fontSize: {
+        xl: '1.5rem',
+        '2xl': '2.5rem',
+        '3xl': '4rem',
+        '4xl': '7rem',
+        '5xl': '8rem',
+        '6xl': '9rem',
+      },
+    },
   },
   plugins: [],
 }
