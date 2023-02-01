ca86c743b1bd4f606315353fd8a16b559abb0e9d
Afeez Awoyemi <damafeez@gmail.com>
2023-02-01 17:51:08 +0100

    feat: improve hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,17 +12,17 @@ export default function Hero({ className = '' }) {
       <div className="flex-1 basis-96">
         <h1
           data-scroll
-          data-scroll-speed="3"
+          data-scroll-speed="2"
           className="text-3xl text-foreground-secondary max-w-xl">
           Building intuitive,
           <br />
           user-friendly applications.
         </h1>
         <p className="text-justify max-w-4xl">
-          I am Afeez Awoyemi, frontend engineer with proven experience in building
-          software solutions, facilitating projects from concept to launch. I am
-          well-versed in modern frontend technologies, eco systems as well as design
-          tools.
+          I am <b className="text-foreground-secondary">Afeez Awoyemi</b>, frontend
+          engineer with proven experience in building software solutions, facilitating
+          projects from concept to launch. I am well-versed in modern frontend
+          technologies, eco systems as well as design tools.
         </p>
       </div>
       <AfeezAvatar className="flex-1 basis-64" />
