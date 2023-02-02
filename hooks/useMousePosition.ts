00df01011b2462058e4f602f16705c445f54f59e
Afeez Awoyemi <damafeez@gmail.com>
2023-02-02 20:15:39 +0100

    feat: show cursor only when mouse is detected

--- hooks/useMousePosition.ts
+++ hooks/useMousePosition.ts
@@ -2,21 +2,25 @@ import { useEffect } from 'react'
 
 export default function useMousePosition() {
   const delay = 50
-  useEffect(() => {
-    const onMouseMove = function (e: MouseEvent) {
-      setTimeout(() => {
-        document.body.style.setProperty(
-          '--mouse-x',
-          (e.clientX + window.scrollX).toString()
-        )
-        document.body.style.setProperty(
-          '--mouse-y',
-          (e.clientY + window.scrollY).toString()
-        )
-      }, delay)
-    }
 
-    document.addEventListener('mousemove', onMouseMove)
-    return () => document.removeEventListener('mousemove', onMouseMove)
+  useEffect(() => {
+    const hasPointer = window.matchMedia('(pointer: fine)')
+    if (hasPointer.matches) {
+      const onMouseMove = function (e: MouseEvent) {
+        setTimeout(() => {
+          document.body.style.setProperty(
+            '--mouse-x',
+            (e.clientX + window.scrollX).toString()
+          )
+          document.body.style.setProperty(
+            '--mouse-y',
+            (e.clientY + window.scrollY).toString()
+          )
+        }, delay)
+      }
+
+      document.addEventListener('mousemove', onMouseMove)
+      return () => document.removeEventListener('mousemove', onMouseMove)
+    }
   })
 }
