8daee6426e30666146ca50632514d3b54606f4a1
Afeez Awoyemi <damafeez@gmail.com>
2023-02-03 11:26:29 +0100

    feat: improvements to AboutMe

--- app/AboutMe.tsx
+++ app/AboutMe.tsx
@@ -7,30 +7,25 @@ export default function About() {
         About Me
       </h2>
       <p>
-        It all began in 2017! My software engineering journey started as a curious
-        interest to understand how computers worked, particularly my tiny Android device,
-        a Google search, an excitement, then several months of learning. Nemo eos rem
-        consequuntur tempora alias laudantium vel voluptas autem impedit a ipsa ut
-        suscipit, qui, eum incidunt. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Nulla temporibus asperiores quo nemo, vero ab voluptatibus dolorum
-        doloremque rerum necessitatibus, aspernatur ipsa consequatur rem, magni sed
-        pariatur ratione similique iure!
+        My software engineering journey started as a curious interest to understand how
+        computers worked, particularly my tiny Android device, a Google search, an
+        excitement, then several months of learning. Nemo eos rem consequuntur tempora
+        alias laudantium vel voluptas autem impedit a ipsa ut suscipit, qui, eum incidunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla temporibus
+        asperiores quo nemo, vero ab voluptatibus dolorum doloremque rerum necessitatibus,
+        aspernatur ipsa consequatur rem, magni sed pariatur ratione similique iure!
       </p>
       <p>
-        Vue resonates very well with the way I think about wiring front-end applications,
-        I believe this is because it was the first front-end framework I learned back in
-        2017; I also love that it is both a simple and intuitive framework (very close to
-        how traditional HTML/JS works). I have encountered a lot of scenarios and solved
-        multiple technical issues from within the many applications I have built with Vue.
-      </p>
-      <p>
-        A friend of mine had such heartwarming comments and excitement when he realized I
+        A friend of mine had such heartwarming comments and excitement when he found out I
         had worked on the application he uses while delivering orders for GoPuff, this is
         why I enjoy web and software development - that you get the chance to impact and
         improve the lives of real people.
       </p>
 
-      <div className="group flex flex-wrap items-center gap-8 mt-24">
+      <div
+        data-scroll
+        data-scroll-speed="2"
+        className="group flex flex-wrap items-center gap-8 mt-24">
         <h3 className="text-xl leading-loose tracking-wide text-foreground-secondary text-opacity-30">
           <span>some</span>
           <br />
