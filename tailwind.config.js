f14dc30904116f4642e3de8648c4dc0126d689fb
Afeez Awoyemi <damafeez@gmail.com>
2023-02-05 16:20:50 +0100

    chore: update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
