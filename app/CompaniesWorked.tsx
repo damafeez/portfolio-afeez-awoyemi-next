1c685490d2a37c5301a30c81e1ab923a8d2e43cd
Afeez Awoyemi <damafeez@gmail.com>
2023-03-02 19:01:01 +0100

    ui improvements

--- app/CompaniesWorked.tsx
+++ app/CompaniesWorked.tsx
@@ -12,35 +12,41 @@ export default function Companies() {
           </h3>
 
           <p>
-            I was fortunate to have had the most fun and exciting experience of my career
-            yet, at GoPuff. I worked alongside many talented individuals in various
-            capacities. Most of the time, I maintained our frontend applications (or spun
-            up brand new ones as required); other times, I was a wildcard, touching on
-            various backend applications, the mobile driver app, creating Azure functions
-            or doing some simple designs.
+            Honoured to have had the most fun and exciting experience of my career yet, at
+            GoPuff. I worked alongside many talented individuals in various capacities.
+            Most of the time, I maintained our frontend applications (or spun up brand new
+            ones as required); other times, I was a wildcard, touching on various backend
+            applications, the mobile driver app, creating Azure functions or doing some
+            simple designs.
           </p>
           <ul>
-            <li>
-              I lead development for a risk monitoring platform allowing administrators to
-              track site metrics proactively. Lead development for an order dispatching
-              application, allowing administrators to view order status and override
-              driver assignments. This provided an easy way to track orders internally and
-              ensure they were appropriately assigned to the proper drivers for the
-              fastest delivery.
+            <li className="mb-3">
+              I was responsible for several key projects that helped streamline operations
+              and improve efficiency across various departments. One of my most
+              significant accomplishments was developing a risk monitoring platform that
+              allowed administrators to track site metrics proactively. This platform
+              provided real-time insights into key performance indicators, enabling teams
+              to make data-driven decisions and reduce risk.
             </li>
-            <li>
-              I built an internal tool that lets managers tweak order grouping parameters
-              with role-based access. The product increased visibility and consistency by
-              allowing us to track who and when changes were made.
+            <li className="mb-3">
+              I led development for an order dispatching application that allowed
+              administrators to view order status and override driver assignments. This
+              provided an easy way to track orders internally and ensure they were
+              appropriately assigned to drivers for the fastest delivery. I also built an
+              interface for delivery partners to track and visualize their delivery
+              metrics, making it easy to see how close they are to qualifying for company
+              benefits. This resulted in increased turnover, which was a significant win
+              for the company.
             </li>
-            <li>
-              I collaborated in rewriting the Order Views section of the customer app in
-              Vue from a legacy application allowing for a more modern, maintainable
-              codebase.
-            </li>
-            <li>
-              I created Azure Serverless Functions integrating with 3rd-party services for
-              driver acquisition and onboarding resulting in overall driver happiness.
+            <li className="mb-3">
+              I built an internal tool that let managers tweak order grouping parameters
+              with role-based access. This product increased visibility and consistency by
+              allowing us to track who and when changes were made. Additionally, I
+              collaborated on rewriting the Order Views section of the customer app in Vue
+              from a legacy application, allowing for a more modern, maintainable
+              codebase. Lastly, I created Azure Serverless Functions that integrated with
+              3rd-party services for driver acquisition and onboarding, resulting in
+              overall driver happiness.
             </li>
           </ul>
         </div>
@@ -57,7 +63,7 @@ export default function Companies() {
             natus sed pariatur vel eligendi. Eos, quaerat!
           </p>
           <ul>
-            <li>
+            <li className="mb-3">
               I collaborated in developing a mobile application for scheduling and
               managing meeting room availability within the company.
             </li>
--- constants/projects.ts
+++ constants/projects.ts
@@ -1,7 +1,7 @@
 import folios from '../public/folios.png'
 import wordle from '../public/wordle.png'
 import wordleMobile from '../public/wordle-mobile.png'
-import simpleMusic from '../public/simple-music.webp'
+import simpleMusic from '../public/simple-music-2.png'
 import fonto from '../public/fonto.png'
 
 const projects = [
@@ -9,36 +9,36 @@ const projects = [
     image: folios,
     title: 'Folios',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam error, fuga alias assumenda commodi reiciendis numquam ducimus atque uaerat modi beatae ratione in unde minus blanditiis hic quidem.',
+      'I had an idea to build an open-source template engine for generating portfolio sites, suitable for devs, designers, photographers and artists. Folios is still work in progress.',
     technologies: ['vue', 'nuxt', 'tailwind'],
-    url: '#',
-    github: '#',
+    url: 'https://folios.page/',
+    github: 'https://github.com/damafeez/portfolio.ng',
   },
   {
     image: fonto,
     title: 'Fonto',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam error, fuga alias assumenda commodi reiciendis numquam ducimus atque uaerat modi beatae ratione in',
+      'I built an image processing tool with Vue and HTML Canvas for overlaying text on photos. Fonto allows you to upload an image, customize the text to ovrlay on it, tweak some parameters until the desired outcome is reached, then download output image.',
     technologies: ['vue', 'html', 'canvas', 'css3'],
-    url: '#',
-    github: '#',
+    url: 'https://fonto.netlify.com/',
+    github: 'https://github.com/damafeez/fonto',
   },
   {
     image: simpleMusic,
     title: 'Simple Music',
     description:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam error, fuga alias assumenda commodi reiciendis numquam ducimus atque uaerat modi beatae ratione in unde minus blanditiis hic quidem.',
-    technologies: ['vue', 'nuxt', 'tailwind'],
-    url: '#',
-    github: '#',
+      'A simple music player with a beautiful interface. A simple music player with a beautiful interface.',
+    technologies: ['flutter', 'dart', 'mobile'],
+    url: 'https://play.google.com/store/apps/details?id=afeez.awoyemi.simple_music_player',
+    github: 'https://github.com/damafeez/simple-music',
   },
   {
     image: wordleMobile,
     title: 'Wordle (mobile)',
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam error, fuga alias assumenda commodi reiciendis numquam ducimus atque uaerat modi beatae ratione in',
-    technologies: ['vue', 'css3'],
-    github: '#',
+    technologies: ['react', 'react-native', 'typescript'],
+    github: 'https://github.com/damafeez/wordle-react-native',
   },
   {
     image: wordle,
@@ -46,8 +46,8 @@ const projects = [
     description:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam error, fuga alias assumenda commodi reiciendis numquam ducimus atque uaerat modi beatae ratione in',
     technologies: ['vue', 'css3'],
-    url: '#',
-    github: '#',
+    url: 'https://wordle-clone.netlify.app/',
+    github: 'https://github.com/damafeez/wordle',
   },
 ]
 
