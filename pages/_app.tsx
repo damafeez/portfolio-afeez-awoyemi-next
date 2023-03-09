b522fe6001c0691478903aded941fd0e63f6a202
Afeez Awoyemi <damafeez@gmail.com>
2023-03-09 21:15:28 +0100

    analytics: add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,31 @@
 import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
-
-import '../styles/cursor.scss'
+import Script from 'next/script'
 import useMousePosition from '../hooks/useMousePosition'
+
+import '../styles/globals.scss'
+import '../styles/cursor.scss'
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
 
 export default function App({ Component, pageProps }: AppProps) {
   useMousePosition()
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ window.dataLayer.push(arguments); }
+          gtag('js', new Date());
+
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
