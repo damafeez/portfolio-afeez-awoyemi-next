c163d9e6f105c238156a3dea667e1a1c53ce8130
Afeez Awoyemi <damafeez@gmail.com>
2023-03-20 14:55:25 +0100

    about: add link to resume

--- app/ScocialLinks.tsx
+++ app/ScocialLinks.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si'
+import { IoDocumentTextOutline } from 'react-icons/io5'
 
 const links = [
   {
@@ -13,14 +14,21 @@ const links = [
   {
     to: 'mailto:damafeez@gmail.com',
     icon: SiGmail,
+    title: 'Email Address',
+  },
+  {
+    to: 'https://docs.google.com/document/d/11ggxtgqYtheNBjMVzrR7qNerCDJ0E9f1lkfZLyxy9Ng/edit?usp=sharing',
+    icon: IoDocumentTextOutline,
+    title: 'Resume',
   },
 ]
 
 export default function SocialLinks({ className = '' }) {
   return (
     <div className={clsx(className, 'flex gap-4')}>
-      {links.map(({ to, icon: Icon }) => (
+      {links.map(({ to, icon: Icon, title }) => (
         <a
+          title={title}
           className="w-14 h-14 border border-solid border-foreground border-opacity-20 flex items-center justify-center rounded-full"
           key={to}
           href={to}
